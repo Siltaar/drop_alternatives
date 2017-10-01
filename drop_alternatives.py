db9f95468cb0d871f304faf7c45ba19f3723de58
Siltaar <siltaar@d12s.fr>
2017-10-01 15:10:24 +0200

    deep rework, now accepts dealing with multiparts. Deletes HTML part even if multiple text parts exists, but only if text parts are not 10 times smallers (as mailling-list signatures can be for instance).

--- drop_alternatives.py
+++ drop_alternatives.py
@@ -3,112 +3,53 @@
 # author : Simon Descarpentries, 2017-09
 # licence: GPLv3
 
-"""
-Copyright: 2017 Simon Descarpentries
-Copyright: 2010-2012 Rogério Theodoro de Brito
-
-`drop_alternatives.py` is a simple Python script for those who hate emails in
-HTML and who prefer their inbox to have as many messages in pure text as
-feasible. This script is generally meant to be run as a filter with procmail
-FDM or some other mail delivery agent.
-
-It tries to be moderately conservative and only act when things are
-moderately safe:
-
-* If the message is `multipart` and has a `text/plain` and a `text/html`
-  part, keep the `text/plain` part only.
-
-* In all other cases keep the message intact.
-"""
 
 import email
-import email.message
+from email.mime.multipart import MIMEMultipart
 
 
-def compose_message(orig, body):
-	"""
-	Create new message with headers from `orig` and body from `body`.
-
-	* `orig`: The original message.
-	* `body`: The body that we want the new message to have.
-	* Returns a new message.
-
-	`compose_message` creates a new message with most of the fields from
-	`orig`, with fields from `body` (if any) and with the payload of
-	`body`. The fields excluded from `orig` are the following:
-
-	* `content-length`
-	* `content-type`
-	* `lines`
-	* `status`
-	"""
-	wanted = email.message.Message()
-	wanted.set_payload(body.get_payload())
-
+def compose_message(orig, parts):
+	wanted = MIMEMultipart()
 	unwanted_fields = ["content-length", "content-type", "lines", "status"]
 
-	# The dictionaries `orig` and `body` have only headers as their items.
 	for field in unwanted_fields:
 		del orig[field]
 
-	for k, v in orig.items():
+	for k, v in orig.items():  # `orig` have only headers as its items
 		wanted[k] = v
-	for k, v in body.items():
-		wanted[k] = v
+
+	for p in parts:
+		wanted.attach(p)
 
 	return wanted
 
 
 def drop_alternatives(msg):
-	"""
-	Given an RFC-2822 message `msg`, generate its 'sanitized' version.
+	if msg.is_multipart():
+		text_parts = []
+		html_parts = []
+		misc_parts = []
 
-	* `msg`: The message to be sanitized.
-	* Returns a sanitized version of `msg`.
+		for part in msg.walk():
+			if (part.get_content_maintype() == "multipart" or
+				part.get_content_type() == "message/external-body" or
+				part.get_payload() == ""):
+				continue
+			elif part.get_content_type() == "text/plain":
+				text_parts.append(part)
+			elif part.get_content_type() == "text/html":
+				html_parts.append(part)
+			else:
+				misc_parts.append(part)
 
-	`drop_HTML_part` tries to be moderately conservative and only act when things
-	are moderately safe:
+		if (html_parts and len(text_parts) >= len(html_parts) and
+			sum([len(a.get_payload()) for a in html_parts]) <
+				10 * sum([len(b.get_payload()) for b in text_parts])):
+			return compose_message(msg, text_parts + misc_parts)
 
-	* If the message is multipart and has a `text/plain` and a `text/html`
-		part, keep the `text/plain` part only.
-
-	* In all other cases keep the message intact.
-	"""
-	if not msg.is_multipart():
-		return msg
-
-	# 'composition' is a bitmask containing the kind of the parts
-	TEXTPLAIN = 1  # text/plain
-	TEXTHTML = 2  # text/html
-	MISCPARTS = 4  # anything else
-
-	composition = 0
-	text_taken = False
-
-	for part in msg.walk():
-		if (part.get_content_maintype() == "multipart" or
-			part.get_content_type() == "message/external-body" or
-			part.get_payload() == ""):
-			continue
-		elif part.get_content_type() == "text/plain":
-			if not text_taken:
-				text_taken = True
-				body = part
-				composition |= TEXTPLAIN
-			else: # if we are seeing a second text/plain part, stop throwing things
-				composition |= MISCPARTS
-				break
-		elif part.get_content_type() == "text/html":
-			composition |= TEXTHTML
-		else:
-			composition |= MISCPARTS
-
-	if (composition & TEXTPLAIN) != 0 and (composition & MISCPARTS) == 0:
-		return compose_message(msg, body)
-	else:
-		return msg
+	return msg
 
 
 if __name__ == "__main__":
 	import sys
-	print(drop_alternatives(email.message_from_file(sys.stdin)).as_string())
+	print(drop_alternatives(email.message_from_file(sys.stdin)))
